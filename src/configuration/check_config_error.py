c64b0d061a62914471d6245d686dac76e7de99d2
alebaron <alizea.lebaron2@gmail.com>
2026-02-09 16:34:28 +0100

    Modification .gitignore + Ajout des premières gestion d'erreurs

--- src/configuration/check_config_error.py
+++ src/configuration/check_config_error.py
@@ -1,26 +1,38 @@
 # ************************************************************************* #
 #                                                                           #
 #                                                      :::      ::::::::    #
-#  error.py                                          :+:      :+:    :+:    #
+#  check_config_error.py                             :+:      :+:    :+:    #
 #                                                  +:+ +:+         +:+      #
 #  By: alebaron, tcolson                         +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
-#  Created: 2026/02/09 16:02:31 by alebaron        #+#    #+#               #
-#  Updated: 2026/02/09 16:29:02 by alebaron        ###   ########.fr        #
+#  Created: 2026/02/09 15:52:15 by alebaron        #+#    #+#               #
+#  Updated: 2026/02/09 16:32:24 by alebaron        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
 # +-------------------------------------------------------------------------+
-# |                                  Class                                  |
+# |                               Importation                               |
 # +-------------------------------------------------------------------------+
 
-class ConfigurationError(Exception):
-	pass
+
+from utils.error import send_error, ConfigurationError
+
 
 # +-------------------------------------------------------------------------+
 # |                                Function                                 |
 # +-------------------------------------------------------------------------+
 
-def send_error(error_type: Exception, message: str) -> None:
-	print(f"{error_type}: {str}")
-	exit(2)
+def check_file(filename: str) -> None:
+
+    try:
+        open(filename, "r")
+    except FileNotFoundError:
+        send_error(ConfigurationError,
+                   f"File \"{filename}\" not found. "
+                   "Check the path and try again.")
+    except PermissionError:
+        send_error(ConfigurationError, f"No permission to access {filename}."
+                   " Check your access rights.")
+    except IsADirectoryError:
+        send_error(ConfigurationError, f"Expected a file, but got directory "
+                   f"{filename}. Please provide a file.")
